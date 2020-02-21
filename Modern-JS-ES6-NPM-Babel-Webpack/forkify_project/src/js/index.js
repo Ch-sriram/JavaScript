2fc6a18d675ab6439debac538332a1a1133a7dce
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-02-21 21:13:04 +0530

    151 Forkify: Search Model - Added 7.7.4 named as "Building the Search Model using ES6 classes"
    README Update
    - master links for ./src/js/index.js, ./src/js/models/Search.js added in Index at 7.7.4

--- Modern-JS-ES6-NPM-Babel-Webpack/forkify_project/src/js/index.js
+++ Modern-JS-ES6-NPM-Babel-Webpack/forkify_project/src/js/index.js
@@ -1,84 +1,19 @@
 /********************************************************************************************************************
  * What we'll learn:
  * ----------------
- * 1. How to make Real World API Calls.
- * 2. What API Keys are why we need them?
+ * 1. How to build simple data model using ES6 Classes.
  * 
- * The API that we were supposed to use was known as "food2fork", but now, that API has shut down its services, and
- * therefore, we will use the 'forkify' API, which is hosted @https://forkify-api.herokuapp.com/
- * 
- * Now, if we were using the food2fork's API, then in there, there was a Recipe API which allowed us to do two things
- * 1. Search for a Recipe.
- * 2. Get Data about a specific Recipe.
- * 
- * For the searching part, we had to make a search request using the API URL: https://food2fork.com/api/search,
- * but now, we will use the following API URL: https://forkify-api.heroku-app.com/api/search
- * 
- * To get the data, we used the other API endpoint which was: https://food2fork.com/api/get, but now, we will use
- * another API endpoint which is the following: https://forkify-api.heroku-app.com/api/get
- * 
- * Using the food2fork API was free for the 500 request, but after that, the API didn't respond. But using forkify
- * API, we can have a 100 requests per hour.
- * 
- * Now, to make any API call, we had to create our own account in food2fork's website, because to make an AJAX
- * request to the API endpoint, we needed to have an API Key. An API Key is like a password that's given to each
- * user which is a unique ID that each user can use in order to make requests, due to which, the API can track the 
- * number of requests the user makes per day. The API Key is basically a long string of alphanumerics which was to
- * be included in the request link along with the search query or the get data query. But now, using forkify-api, 
- * we need not use any API Keys to search or get the data.
- * 
- * Now, we will make an API call to fetch the data from the forkify API.
- * 
- * NOTE ABOUT fetch():
- * ------------------
- * Previously, we used the fetch() API to make AJAX Requests and it is fine to work with in the latest browsers.
- * But the fetch() API doesn't really work with all the browsers yet. A very old browser, might not recognize
- * the fetch() API call. Instead, what we are going to do is to use an extremely popular HTTPRequest Library called
- * AXIOS. To use axios(), we need to install it and get it from NPM, using 'npm install axios --save' (note that
- * we save axios as a code dependency, and not as a dev dependency, because we use its implementation in our code).
- * And then we import the axios package as: import axios from 'axios'; wherever we want to use axios. Conventionally,
- * the package is always imported as "axios". Now, we don't need to provide the path because webpack will just
- * recognize the path when bundling the modules together when it sees the 'axios'. Now, axios() call works exactly
- * the same way as fetch() does, but axios() doesn't return a string in JSON format, it automatically returns a 
- * JSON object, whereas using fetch(), we had to convert the JSON formatted string that we got into a JSON object.
- * Therefore, that extra step of converting the string returned from the fetch() to a JSON object, is removed here.
- * One more thing about axios() is that, it better at error handling compared to the fetch() API.
- * 
- * We will see how we use axios() below
+ * For the search query, we are going to describe the data model using ES6 classes inside the 
+ * ./src/js/models/Search.js file. Therefore, inside Search.js, the only thing we export is the class. 
+ * Inside Search.js, we will have a class called Search, which is exported and that class takes in a parameter in 
+ * its constructor called query, because that's what the query given by the user is. The query is taken from the user
+ * and provided to the getResult function inside the Search class (Open the Search class to check the code).
  */
 
 import axios from 'axios';
+import Search from './models/Search';
 
-async function getResult(query) {
-    // Previously, we had to have a key and only then, we could query the food2fork API. An AJAX Request would be
-    // as follows: (we'd need the CORS proxy to use the API, such as: https://cors-anywhere.herokuapp.com/)
-    // const proxy = 'https://cors-anywhere.herokuapp.com/';
-    // const key = '1984bi1219823ibd83412';
-// The foll. line of code will work, if we call the getResult() function with a search query say getResult('pizza');
-    // const result = await axios(`${proxy}https://food2fork.com/api/search?key=${key}&q=${query}`);
-    // console.log(result); 
-
-    try {
-        // Now, we don't need any key for the search, we simply use the forkify() API as follows:
-        const key = '20200221195421';
-        const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-        //console.log(result);    // from the result we get, we only want the data field inside which the recipe 
-                                  // array contains the recipes
-        const recipes = result.data.recipes;
-        console.log(recipes);
-        // In every element of the recipes array, we have an object in which there are details about
-        // the publisher, title, source_url, recipe_id, image_url, social_rank, publisher_url, etc, out of which
-        // we'll require the "recipe_id" field to get the data about that recipe when we want to use the 
-        // get data service using the https://forkify-api.heroku-app.com/api/get. We have to append the
-        // recipe_id as follows: https://forkify-api.heroku-app.com/api/get?rId=47746, to get the data for the pizza
-        // recipe.
-    } catch (error) {
-        console.log(error);
-        alert(error);
-    }
-}
-
-getResult('pizza');
-getResult('pasta');
-getResult('curry');
-getResult('paneer');  // error generated for this request because it is not a search phrase supported in forkify-api
+const search = new Search('pizza');
+console.log(search);
+search.getResult(); // for this example, we have hard-coded the search to 'pizza', later, we will get the query from
+                    // the DOM
--- Modern-JS-ES6-NPM-Babel-Webpack/forkify_project/src/js/models/Search.js
+++ Modern-JS-ES6-NPM-Babel-Webpack/forkify_project/src/js/models/Search.js
@@ -1,1 +1,21 @@
-export default "I am an exported string.";
+import axios from 'axios';  // We need axios to make an AJAX Request
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResult() {    // we don't need to use the function keyword to define a function inside the class
+        try {
+            // Instead of query, we use this.query in the AJAX request.
+            const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
+            
+            //We want to get the recipe from the AJAX Request, but we want it to be stored inside this Search instance
+            this.recipes = result.data.recipes;
+            console.log(this.recipes);
+        } catch (error) {
+            console.log(error);
+            alert(error);
+        }
+    }
+}
